ba89c52cb4b3fdd9130ee420f25a4887b38a1616
Kirsty Alexandra Nguegang <kirstyalexandra.nguegang471@myci.csuci.edu>
2018-04-19 15:28:44 -0700

    done

---
+++ src/Lab12/CheckInventory.java
@@ -0,0 +1,58 @@
+package Lab12;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * @author atb
+ * @version 4/17/2018
+ * @modifiedBy Kirsty Alexandra Nguegang
+ */
+public class CheckInventory
+{
+    // TODO Project 2 Part1
+
+    // uncomment main when the class
+    // skeleton is in place
+
+
+//    public static void main(String[] args)
+//    {
+//        String receivedFile = "randomFile.txt";
+//        String sentFile = "sortedFile.txt";
+//        String corruptedFile = "corruptedFile.txt";
+//        CheckInventory checker = new CheckInventory();
+//
+//        try
+//        {
+//            System.out.println("How many CarIDs to generate?");
+//            Scanner keyboard = new Scanner(System.in);
+//            int amount = keyboard.nextInt();
+//            TreeSet<CarID> sortedSet = checker.generateContentAndSaveToRandomFile(amount, receivedFile);
+//            checker.saveSortedContentToSortedFile(sortedSet, sentFile);
+//            checker.createCorruptedFile(sortedSet, corruptedFile);
+//            System.out.println("\n*** Checking if \"" + sentFile + "\" and \"" + receivedFile + "\" have the same elements ***");
+//            boolean same = checker.compareInventory(receivedFile, sentFile);
+//            System.out.println("--> the elements in files \"" + receivedFile
+//                    + "\" and \"" + sentFile
+//                    + " are " + (same ? "" : "NOT ") + "the same");
+//
+//
+//            System.out.println("\n*** Checking if \"" + sentFile + "\" and \"" + corruptedFile + "\" have the same elements ***");
+//            checker.createHashedDictionary();
+//            same = checker.compareInventory(sentFile, corruptedFile);
+//            System.out.println("--> the elements in files \"" + corruptedFile
+//                    + "\" and \"" + sentFile
+//                    + " are " + (same ? "" : "NOT ") + "the same");
+//
+//        } catch (IOException ioe)
+//        {
+//            System.out.println("There was an error in reading or opening the file: ");
+//            System.out.println(ioe.getMessage());
+//        } catch (InputMismatchException ime)
+//        {
+//            System.out.println(ime.getMessage());
+//        }
+//        System.out.println("\nBye!");
+//    }  // end main
+}
